7ba2f7cdc1b3f65b8ef953b04a907f9cdb98cf9f
David <dsi28@hotmail.com>
2019-01-15 12:51:29 -0500

    first problem done

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,6 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+   return (1/Math.sqrt(5)) *  ((Math.pow(((1+Math.sqrt(5))/2),n)) - (Math.pow(((1-Math.sqrt(5))/2),n)))
 }
+console.log('Fibo: '+fib(7));
