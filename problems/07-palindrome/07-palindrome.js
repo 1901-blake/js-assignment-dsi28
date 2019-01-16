285722ef326c0d59b79d482c7a0eef26431b0adb
David <dsi28@hotmail.com>
2019-01-15 23:29:40 -0500

    problem 7

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,10 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    someStr = someStr.replace(/ /g, '');
+    if(someStr === someStr.split("").reverse().join("")){
+        return true;
+    }else return false;
 }
+let pal= 'madam';
+console.log(isPalindrome(pal));
