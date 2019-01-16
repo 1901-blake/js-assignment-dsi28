7af02390eb296b10fbe94c219ea223abce450c4c
David <dsi28@hotmail.com>
2019-01-15 23:12:35 -0500

    probelm 6

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,8 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    if(someNum & 1 ==1){ // odd
+        return false;
+    }else return true;
 }
+console.log(isEven(8));
