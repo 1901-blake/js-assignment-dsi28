23171dbb7cd38057756da3d79edef61d46086d5a
David <dsi28@hotmail.com>
2019-01-16 19:19:00 -0500

    problem 9

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,19 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
-Print every property and it's value. */
+Print every property and it's value.
+forin loop
+*/
 function objectProperties(someObj) {
-
+    for (const key in someObj) {
+        if (someObj.hasOwnProperty(key)) {
+           console.log('name: '+ key+ '\tvalue:'+someObj[key]);            
+        }
+    }
 }
+let obj= {
+    prop1: 'prop1',
+    prop2: 2,
+    prop3: true,
+    prop4: ['41', 42, false]
+}
+objectProperties(obj);
