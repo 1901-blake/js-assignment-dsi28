468d9df50b6934671234bb52722d9809ab18d115
David <dsi28@hotmail.com>
2019-01-17 13:44:47 -0500

    changed prob1 code

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,6 +2,21 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-   return (1/Math.sqrt(5)) *  ((Math.pow(((1+Math.sqrt(5))/2),n)) - (Math.pow(((1-Math.sqrt(5))/2),n)))
+   //return Math.round((1/Math.sqrt(5)) * ((Math.pow(((1+Math.sqrt(5))/2),n))-(Math.pow(((1-Math.sqrt(5))/2),n))));
+   let last = 0;
+   let fibo =0;
+   if(n === 0 || n===1){
+      last = n;
+      fibo = n;
+   }else if(n>=2 ){
+      last =1;
+      fibo=1;
+      for (let i = 2; i < n; i++) {
+         let temp = fibo;
+         fibo += last;
+         last= temp;
+      }   
+   }
+   return fibo;
 }
-console.log('Fibo: '+fib(7));
+console.log('Fibo: '+fib(0));
