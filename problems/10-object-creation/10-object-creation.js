f3382c20df09b687c3e98be922b0f39e4e6026a0
David <dsi28@hotmail.com>
2019-01-16 19:30:01 -0500

    probelm 10

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,22 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+let student = {
+    name: 'John',
+    age: 22
+}
+function Student(name, age){
+    this.name=name;
+    this.age= age;
+}
+class StudentClass{
+    constructor(name, age){
+        this.name=name;
+        this.age=age;
+    }
+}
+let x = new Student('ConsName', 22);
+let y = new StudentClass('ClassName', 22);
+console.log(`object literal: ${student}
+constructor: ${x}
+class: ${y}`);
